d9c6b4a31e48ca048ca9c3030127875dab8bafd0
Atle Magnussen <atlemagnussen@gmail.com>
2024-06-08 01:14:10 +0200

    ad console

---
+++ TestConsoleAppDocker/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var fromDate = DateTime.Parse("2018-12-10T00:00:00Z");
+Console.WriteLine($"Fromdate kind = {fromDate.Kind}, value={fromDate:u}");
+
+
+fromDate = fromDate.ToUniversalTime();
+Console.WriteLine($"Fromdate kind = {fromDate.Kind}, value={fromDate:u}");
+
+
+fromDate = fromDate.ToUniversalTime();
+Console.WriteLine($"Fromdate kind = {fromDate.Kind}, value={fromDate:u}");

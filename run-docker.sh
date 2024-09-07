88e05d7aca33c84fde012be2d5dd64ca3592c584
Atle Magnussen <atlemagnussen@gmail.com>
2024-09-07 23:29:14 +0200

    k deploi

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker run --rm -p 8080:8080 --name test-net8-api-container test-net8-api:latest
+docker run --rm -p 8080:8080 --name test-net8-api-container test-net8-web:latest
 
